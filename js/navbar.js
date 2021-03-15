b542ed51bad68c648203f37e166b0c2e9fd3f83e
amine <saissihassaniamine@gmail.com>
2021-03-15 20:55:59 +0100

    end the style

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+function menu(me) {
+    if (me.checked) {
+        document.getElementById("navhide").style.display = "grid";
+    } else {
+        document.getElementById("navhide").style.display = "none";
+    }
+}
